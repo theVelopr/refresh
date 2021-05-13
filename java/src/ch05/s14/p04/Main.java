3a55d94e795ee7ae49b6aa9f396211f7283d12c2
theVelopr <immortaldk@gmail.com>
2021-05-13 10:49:58 +0900

    added: thread and daemon thread information.

--- java/src/ch05/s14/p04/Main.java
+++ java/src/ch05/s14/p04/Main.java
@@ -5,6 +5,25 @@ package ch05.s14.p04;
  * - 다른 스레드가 모두 종료될 경우, 스스로 종료되는 스레드 (정의)
  * - 무한 루프로 대기하면서 동작하는 구현이 많음 (활용)
  *  - 일정 시간마다 동작, interrup등에 의해서 동작
+ *
+ * 정의
+ * - 사용자가 직접적으로 제어하지 않고, 백그라운드에서 돌면서 여러 작업을 하는 프로그램을 말한다.
+ *   시스템 로그를 남기는 syslogd처럼 보통 데몬을 뜻하는 ‘d’를 이름 끝에 달고 있으며, 일반적으로 프로세스로 실행된다.
+ */
+
+
+/**
+ * Thread
+ * - 프로세스 내에서 실행되는 흐름의 단위를 말한다.
+ * - Every thread has a name for identification purposes.
+ *   More than one thread may have the same name.
+ *   If a name is not specified when a thread is created, a new name is generated for it.
+ *
+ * Creating a new thread of execution
+ * 1. to declare a class to be a subclass of Thread. This subclass should override the run method of class Thread.
+ *    An instance of the subclass can then be allocated and started.
+ * 2. to declare a class that implements the Runnable interface. That class then implements the run method.
+ *    An instance of the class can then be allocated, passed as an argument when creating Thread, and started.
  */
 
 class AutoSaver extends Thread {
