e50594862dfb51f8b69bf3c0cdb61de20ece7b98
theVelopr <immortaldk@gmail.com>
2021-05-21 09:23:24 +0900

    added: Interface example.

---
+++ java/src/ch04/s02/p01/ArithmaticOperator.java
@@ -0,0 +1,7 @@
+package ch04.s02.p01;
+
+public interface ArithmaticOperator {
+    int operate(int x, int y);
+    long operate(long x, long y);
+    double operate(double x, double y);
+}
---
+++ java/src/ch04/s02/p01/Divide.java
@@ -0,0 +1,18 @@
+package ch04.s02.p01;
+
+public class Divide implements ArithmaticOperator{
+    @Override
+    public int operate(int x, int y) {
+        return x / y;
+    }
+
+    @Override
+    public long operate(long x, long y) {
+        return x / y;
+    }
+
+    @Override
+    public double operate(double x, double y) {
+        return x / y;
+    }
+}
